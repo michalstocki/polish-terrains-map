38a8e9d8a1aafc036c1fe89ece52018d5715e1ec
Michał Stocki <michal.stocki@uxpin.com>
2018-01-20 00:46:09 +0100

    Development server moved to `server/development` directory

--- server/development/index.js
+++ server/development/index.js
@@ -13,7 +13,7 @@ const server = http.createServer((request, response) => {
 });
 
 function respondWithStatic(url, response) {
-  fs.readFile(path.join(__dirname, `./static${url}`), (err, data) => {
+  fs.readFile(path.join(__dirname, `../../static${url}`), (err, data) => {
     if (err) {
       response.writeHead(404);
       response.end();
@@ -30,7 +30,7 @@ function respondWithHTML(response) {
   response.writeHead(200, {
     'Content-Type': 'text/html',
   });
-  fs.readFile(path.join(__dirname, './static/index.html'), (err, data) => {
+  fs.readFile(path.join(__dirname, '../../static/index.html'), (err, data) => {
     if (err) throw err;
     response.end(data);
   });
